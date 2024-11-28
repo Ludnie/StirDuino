4e293707500b2d50abf4606023ddcb6abd3f15ac
Ludnie <ludwig.gabler@gmail.com>
2024-11-28 18:23:05 +0100

    tidy up and add comments

--- Firmware/StirDuino Firmware/src/main.cpp
+++ Firmware/StirDuino Firmware/src/main.cpp
@@ -1,73 +1,105 @@
+/*****************************************************************************************
+*  ____  ____  __  ____  ____  _  _  __  __ _   __  
+* / ___)(_  _)(  )(  _ \(    \/ )( \(  )(  ( \ /  \ 
+* \___ \  )(   )(  )   / ) D () \/ ( )( /    /(  O )
+* (____/ (__) (__)(__\_)(____/\____/(__)\_)__) \__/ 
+*
+* Firmware for Arduino based magnetic stirrer featuring PI feedback loop and GUI (OLED). 
+* Compatible with Arduino Nano and Nano Every.
+*
+* Code written by Ludwig Gabler
+*****************************************************************************************/
+
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 // Pins
-#define ENC 8   // Optical Encoder with 30 pulses/rev
-#define EN 3    // Pin to enable motor driver (pwm) !!! NOT 2, because this pin has no pwm !!!
-#define PH 2    // Pin to set direction
-#define POT A0  // Abnehmer des Potentiometers
+#define OPTICAL_ENC 8     // Optical Encoder
+#define EN 3      // Pin to enable motor driver (pwm) !!! NOT 2, because this pin has no pwm !!!
+#define PH 2      // Pin to set direction
+#define POT A0    // Wiper terminal of 10k potentiometer
+// #define ENC_SW 7  // Switch of rotary encoder switch
+// #define ENC_DT 6  // DT Pin of rotary encoder switch
+// #define ENC_CLK 5 // CLK Pin of rotary encoder switch
 
-#define MAX_RPM 1400  // maximale Drehzahl. Bei zu hoher Drehzahl können die Encoder-Ticks nicht mehr registriert werden.
-#define MIN_RPM 0     // minimale Drehzahl. Zu niedrige Drehzahlen können instabil sein.
-#define CONTROLLER_REFRESH_RATE 200 // Hz
+#define OPTICAL_ENC_PULSES 60       // number of pulses of optical encoder
+#define MAX_RPM 1400                // maximum speed. If the speed is too high, the encoder ticks can no longer be registered.
+#define MIN_RPM 0                   // mminimum speed. Speeds that are too low can be unstable.
+#define CONTROLLER_REFRESH_RATE 200 // Frequency for updating the PI control values
 
 // Display
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 32
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 #define DISPLAY_REFRESH_RATE 8 // refreshs per second
 
-// put function declarations here:
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+/******************************************************************************* 
+ * Sets motor speed and direction through the use of analogWrite and digitalWrite.
+ *
+ * Inputs:
+ * - direction (1: CW, -1: CCW)
+ * - PWM duty cycle (0 - 255)
+ * - PWM pin
+ * - direction pin
+*******************************************************************************/
 void setMotor(int dir, int pwmVal, int pwm, int phase);
+
+/*******************************************************************************
+ *  Increases position counter by one. Called by interrupt attached to optical
+ * encoder.
+********************************************************************************/
 void readEncoder();
 
 // globals
-int pos = 0;
-float v = 0;
-int pwr = 0;
+int pos = 0;  // tick counter of optical encoder
+float v = 0;  // unfiltered rotational speed (rpm)
+int pwr = 0;  // PWM duty cycle (0 - 255)
 
-long prevT = 0;
-int posPrev = 0;
+long prevT = 0;   // last time PI loop ran (µs)
+int posPrev = 0;  // last tick count of optical encoder
 
-// volatile for variables used in an interrupt
-volatile int pos_i = 0;
+float vt = 0;     // target rotational speed set by potentiometer (rpm)
+float vFilt = 0;  // filtered rotational speed (rpm)
+float vPrev = 0;  // last rotational speed (rpm)
 
-float vt = 0;
-float vFilt = 0;
-float vPrev = 0;
+float eintegral = 0;  // integral term of PI controller
 
-float eintegral = 0;
+volatile int pos_i = 0; // volatile for variables used in an interrupt
 
-const int avrgCount = 64;
+// variables for moving average calculation for more stable values
+// displayed on OLED display
+const int avrgCount = 64;         // sampling width of mooving average
+float vtAvrgBuffer[avrgCount];    // buffer for target speed average
+int vtNextAvrg = 0;               // sample count for target speed average
+float vtAvrg = 0;                 // target speed average
+float vFiltAvrgBuffer[avrgCount]; // buffer for filtered speed average
+int vFiltNextAvrg = 0;            // sample count for filtered speed average
+float vFiltAvrg = 0;              // filtered speed average
 
-float vtAvrgBuffer[avrgCount];
-int vtNextAvrg = 0;
-float vtAvrg = 0;
-
-float vFiltAvrgBuffer[avrgCount];
-int vFiltNextAvrg = 0;
-float vFiltAvrg = 0;
 
 void setup() {
 
-  // Set PWM frequency to 64.5 kHz (also affects tone())
+  // Change devider for clock to set PWM frequency to 64.5 kHz
+  // (also affects tone() if used)
   TCB1_CTRLA = 0b00000001;
 
   Serial.begin(115200);
   Wire.begin();
 
-  pinMode(ENC, INPUT);
+  pinMode(OPTICAL_ENC, INPUT);
   pinMode(EN, OUTPUT);
   pinMode(PH, OUTPUT);
   pinMode(POT, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(ENC), readEncoder, RISING);
+  // Interrupt for optical encoder
+  attachInterrupt(digitalPinToInterrupt(OPTICAL_ENC), readEncoder, RISING);
 
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);  
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
   display.setRotation(2);
   display.display();
   display.clearDisplay();
@@ -77,27 +109,25 @@ void setup() {
 
 void loop() {
 
+  // run PI loop between display refreshs
   for (int i = 0; i <= (CONTROLLER_REFRESH_RATE / DISPLAY_REFRESH_RATE); i++) {
+    
     pos = 0;
-    // ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    //   pos = pos_i;
-    // }
-
     pos = pos_i;
 
-    long currT = micros();
-    float deltaT = ((float) (currT - prevT)) / 1.0e6;
-    float velocity = (pos - posPrev) / deltaT;
+    long currT = micros();                              // current time in µs
+    float deltaT = ((float) (currT - prevT)) / 1.0e6;   // time since last loop in s
+    float velocity = (pos - posPrev) / deltaT;          // current rotational speed (pulses per second)
     posPrev = pos;
     prevT = currT;
 
-    // Convert counts/s to RPM
-    v = velocity / 60.0 * 60.0;
+    v = velocity / OPTICAL_ENC_PULSES * 60.0;           // Convert counts/s to RPM
 
-    // Low-pass filter (25 Hz cutoff)
+    // Low-pass filter (25 Hz cutoff) (should be recalculated with actulal data)
     vFilt = 0.854*vFilt + 0.0728*v + 0.0728*vPrev;
     vPrev = v;
 
+    // moving averages for more stable readings on display
     vFiltAvrgBuffer[vFiltNextAvrg++] = vFilt;
     if (vFiltNextAvrg >= avrgCount) {
       vFiltNextAvrg = 0;
@@ -108,14 +138,14 @@ void loop() {
     }
     vFiltAvrg /= avrgCount;
 
-    // Set target velocity
-    //float vt = 100*(sin(currT/1e6)>0);
+    // calculate target speed from potentiometer reading (noisy)
     int pot = analogRead(POT);
     vt = map(pot, 0, 1023, MIN_RPM, MAX_RPM + 4);
-    if (vt < 10) {
+    if (vt < 10) {  // avoid too low speeds
       vt = 0;
     }
 
+    // calculate moving average for target speed
     vtAvrgBuffer[vtNextAvrg++] = vt;
     if (vtNextAvrg >= avrgCount) {
       vtNextAvrg = 0;
@@ -143,7 +173,7 @@ void loop() {
     if (pwr > 255) {
       pwr = 255;
     }
-    if (vt < 10) {
+    if (vt < 10) {      // avoid too low speeds
       pwr = 0;
     }
     setMotor(dir, pwr, EN, PH);
@@ -151,6 +181,7 @@ void loop() {
     delay(1000 / CONTROLLER_REFRESH_RATE); // Update values every 10 ms
   }
   
+  // update display
   display.clearDisplay();
   display.setCursor(80, 2);
   display.setTextSize(1);
@@ -163,6 +194,8 @@ void loop() {
   display.print(vFiltAvrg, 0);
   display.display();
 
+  // send values over serial interface
+  // formatted for use with serial-plotter (by Mario Zechner)
   Serial.print(">pos:");
   Serial.print(pos);
   Serial.print(",v:");
@@ -183,10 +216,10 @@ void loop() {
 void setMotor(int dir, int pwmVal, int pwm, int phase) {
   analogWrite(pwm, pwmVal);
   if (dir == 1) {
-    digitalWrite(PH, HIGH);
+    digitalWrite(phase, HIGH);
   }
   else if (dir == -1) {
-    digitalWrite(PH, LOW);
+    digitalWrite(phase, LOW);
   }
 }
 
